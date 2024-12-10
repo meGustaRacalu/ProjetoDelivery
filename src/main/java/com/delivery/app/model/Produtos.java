92c15fc492fe7560741cec950d947f84ed1c1a8f
lucasunzaga <lucasunzaga87@gmail.com>
2024-12-10 12:40:17 -0300

    Revisão das classes sobre produtos e relação com a classe categoria

---
+++ src/main/java/com/delivery/app/model/Produtos.java
@@ -0,0 +1,73 @@
+package com.delivery.app.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table( name = "tb_produtos")
+public class Produtos {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+		
+	@NotNull
+	@Size(max = 255)
+	@Size(min = 5, max = 100, message = "O atributo título deve conter no mínimo 05 e no máximo 100 caracteres")
+	private String nome;
+	
+	@NotNull(message = "O atributo preço é Obrigatório!")
+	private float preco;
+	
+	@NotNull(message = "O atributo entrega é Obrigatório!")
+	private float entrega;
+		
+		@ManyToOne
+	    @JsonIgnoreProperties("produtos") 
+	    private Categoria categoria;
+		
+		
+		
+		public Long getId() {
+			return id;
+		}
+		public void setId(Long id) {
+			this.id = id;
+		}
+		public String getNome() {
+			return nome;
+		}
+		public void setNome(String nome) {
+			this.nome = nome;
+		}
+		public float getPreco() {
+			return preco;
+		}
+		public void setPreco(float preco) {
+			this.preco = preco;
+		}
+		public float getEntrega() {
+			return entrega;
+		}
+		public void setEntrega(float entrega) {
+			this.entrega = entrega;
+		}
+		public Categoria getCategoria() {
+			return categoria;
+		}
+		public void setCategoria(Categoria categoria) {
+			this.categoria = categoria;
+		}
+		
+		
+		
+	}
+
---
+++ src/main/java/com/delivery/app/repository/ProdutosRepository.java
@@ -0,0 +1,13 @@
+package com.delivery.app.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.delivery.app.model.Produtos;
+
+public interface ProdutosRepository extends JpaRepository <Produtos, Long> {
+
+	List<Produtos> findAllByNomeContainingIgnoreCase(String nome);
+	
+	}	
