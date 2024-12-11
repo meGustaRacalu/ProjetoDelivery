b42957070d614b97ca4e33c0ce5b97becc966888
lucasunzaga <lucasunzaga87@gmail.com>
2024-12-11 14:30:57 -0300

    mudancinha no CategoriaRepository

--- src/main/java/com/delivery/app/repository/CategoriaRepository.java
+++ src/main/java/com/delivery/app/repository/CategoriaRepository.java
@@ -3,12 +3,13 @@ package com.delivery.app.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.delivery.app.model.Categoria;
 
 public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
 
-    List<Categoria> findAllByDescricaoContainingIgnoreCase(String descricao);
+    List<Categoria> findAllByDescricaoContainingIgnoreCase(@Param("descricao")String descricao);
 
     Categoria findByDescricao(String descricao);  
 }
